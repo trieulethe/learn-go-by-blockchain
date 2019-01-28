c790f0cf118031abc16b7670f4d4e1740687506c
trieult <trieult@kaopiz.com>
2019-01-28 17:25:13 +0700

    init transaction

---
+++ transaction.go
@@ -0,0 +1,18 @@
+package main
+
+type TXInput struct {
+	Txid      []byte
+	Vout      int
+	ScriptSig string
+}
+
+type TXOutput struct {
+	Value        int
+	ScriptPubKey string
+}
+
+type Transaction struct {
+	ID   []byte
+	Vin  []TXInput
+	Vout []TXOutput
+}
